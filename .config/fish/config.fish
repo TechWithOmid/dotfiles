46fca74af7502e59e9b02eb0ea7ed9f2af5d01f6
omid <omidmmadi@gmail.com>
2022-02-23 22:13:38 +0330

    Adding New Configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# npm config
+set NPM_PACKAGES "$HOME/.npm-packages"
+set PATH $PATH $NPM_PACKAGES/bin
+set MANPATH $NPM_PACKAGES/share/man $MANPATH  
+
+# alias
+alias ls "exa --icons"
+alias lsa "exa --icons -a"
+alias ll "exa -l -g --icons"
+alias lla "ll -a"
+alias v "nvim"
+
+# dotfiles alias
+alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+export LANG=en_US.UTF-8
+export PATH="/home/omid/.local/bin:$PATH"
---
+++ .config/i3status/README.md
@@ -0,0 +1,7 @@
+# i3status Docs
+
+This area is a part of [i3 config files](https://github.com/mahdymirzade/dotfiles/tree/main/config/i3).
+
+For documentation, please refer to _config/i3_.
+
+[Main Documentation](https://i3wm.org/docs/userguide.html#_configuring_i3bar)
